de737dde7269c2a9481945755c3130bd80b2b498
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2021-09-12 17:45:11 +0200

    add: player animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float velocity = 2.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // go up if space or main key clicked
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
